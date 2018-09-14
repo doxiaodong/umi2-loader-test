607691ccd94daaa406689a75f7e99cc11df77d3d
duxiaodong <duxiaodong@darlin.me>
2018-09-14 17:25:42 +0800

    fix route

--- pages/home/index.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import template from './index.pug';
-
-export default () => {
-  if (typeof template === 'function') {
-    return <div dangerouslySetInnerHTML={{ __html: template() }} />;
-  }
-  return <>test</>;
-};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import Home from '../pages/home';
+import Home from '../pages/index';
 
 ReactDOM.render(<Home />, document.getElementById('app'));
